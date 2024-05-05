450e17b85cabe08de5f9691f5b2a1e42b293fe26
Vaphieen <Vaphieen@gmail.com>
2024-05-05 12:46:48 +0200

    Added text box and non functional send button

--- app/src/main/java/com/example/androidapp/controllers/ToolbarController.kt
+++ app/src/main/java/com/example/androidapp/controllers/ToolbarController.kt
@@ -2,7 +2,6 @@ package com.example.androidapp.controllers
 
 import android.app.Activity
 import android.view.View
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.androidapp.R
 
@@ -12,16 +11,23 @@ object ToolbarController {
         val plusButton: View = activity.findViewById(R.id.imageButton)
         val parentLayout: ViewGroup = activity.findViewById(R.id.main)
 
+
         plusButton.setOnClickListener {
             // Inflate chat_window.xml layout
-            val inflater = LayoutInflater.from(activity)
-            val chatWindowLayout = inflater.inflate(R.layout.chat_window, parentLayout, false)
+           // val inflater = LayoutInflater.from(activity)
+           // val chatWindow = inflater.inflate(R.layout.chat_window, parentLayout, false)
 
             // Clear existing views from the parent layout
             parentLayout.removeAllViews()
 
             // Add chat_window.xml layout to the parent layout
-            parentLayout.addView(chatWindowLayout)
+           // parentLayout.addView(chatWindow)
+
+
+
+
+
+
         }
     }
 }
